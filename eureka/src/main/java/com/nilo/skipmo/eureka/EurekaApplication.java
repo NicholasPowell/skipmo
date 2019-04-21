a7ca90197c027a1ae294e38692634231547dd321
NicholasPowell <npowell@directvetmarketing.com>
2019-04-20 20:33:29 -0400

    Adding eureka server

---
+++ eureka/src/main/java/com/nilo/skipmo/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.nilo.skipmo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("EUREKA!!");
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
