eb85da88403abb2a0fd1ee80507780e84cfc6547
Nick Powell <nicholas.powell@gmail.com>
2019-06-29 21:28:22 -0400

    tidying up game-application to route incoming requests to correct context - a single game!

--- game-application/src/main/kotlin/com/nilo/skipmo/SkipmoApplication.kt
+++ game-application/src/main/kotlin/com/nilo/skipmo/SkipmoApplication.kt
@@ -2,20 +2,9 @@ package com.nilo.skipmo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.context.annotation.Bean
-import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-//@RibbonClient(name = "skipmo")
-open class SkipmoApplication {
-    @LoadBalanced
-    @Bean
-    open fun restTemplate(): RestTemplate {
-        return RestTemplate()
-    }
-
-}
+open class SkipmoApplication
 
 fun main(args: Array<String>) {
     runApplication<SkipmoApplication>(*args)
